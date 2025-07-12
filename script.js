9a7b25050cebd652a42ca2c56941077dd79d9a5a
JANIER-VSQZ <vasquezjanier31@gmail.com>
2025-07-11 18:45:42 -0600

    boton se hace mas grande

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnNo = document.getElementById('btnNo');
     const btnSi = document.getElementById('btnSi');
     const frente = document.getElementById('frente');
+    let contador = 1.5;
 
     btnNo.addEventListener('click', () => {
         let randomX = Math.floor(Math.random() * 100);
         let randomY = Math.floor(Math.random() * 100);
         btnNo.style.left = randomX + '%';
         btnNo.style.top = randomY + '%';
+        contador=contador*2;
+        btnSi.style.transform = 'scale('+contador+')';
+        btnSi.style.zIndex=2;
     })
 
     btnSi.addEventListener('click', ()=>{
